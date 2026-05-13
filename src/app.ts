0ae200ced9e8b3e6213abc3036331d739540fb27
mentorvikash <mentorvikash@gmail.com>
2026-05-13 10:07:01 +0530

    feat: implement database connection logic and update server startup process

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+
 import { env } from "./config/env";
+import { connectDB } from "./config/db";
 
 const app = express();
 
@@ -18,6 +20,23 @@ app.get("/health", (req, res) => {
   });
 });
 
-app.listen(env.PORT, () => {
-  console.log(`Server is running on port ${env.PORT}`);
+const startServer = async () => {
+  // connect to database
+  await connectDB();
+
+  // start the server
+  app.listen(env.PORT, () => {
+    console.log(`Server is running on port ${env.PORT}`);
+  });
+};
+
+startServer();
+
+// graceful shutdown
+process.on("SIGINT", async () => {
+  console.log("shutting down server gracefully");
+  await import("mongoose").then((mongoose) =>
+    mongoose.default.connection.close(),
+  );
+  process.exit(0);
 });
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,1 +1,24 @@
 import mongoose from "mongoose";
+import { env } from "./env";
+
+export const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(env.MONGO_URI);
+    console.log("Mongodb successfully connected");
+  } catch (error: Error | unknown) {
+    if (error instanceof Error) {
+      console.error("Errror connecting to mongodb", error.message);
+    }
+    process.exit(1);
+  }
+};
+
+mongoose.connection.on("connected", () => {
+  console.log("mongodb connection established");
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("mongodb connection error", err);
+});
