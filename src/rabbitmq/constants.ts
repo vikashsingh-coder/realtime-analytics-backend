7b83f4a418293ba3b0e8498449658abaedc4a731
mentorvikash <mentorvikash@gmail.com>
2026-05-23 23:09:13 +0530

    feat: add RabbitMQ constants for exchanges, routing keys, and queues

---
+++ src/rabbitmq/constants.ts
@@ -0,0 +1,14 @@
+export const EXCHANGE = {
+  ANALYTICS: "analytics.exchange",
+};
+
+export const ROUTING_KEY = {
+  ALERT_CREATED: "alert.created",
+  EMAIL_SENT: "email.sent",
+};
+
+export const QUEUE = {
+  ALERT_QUEUE: "ALERT_QUEUE",
+  EMAIL_QUEUE: "EMAIL_QUEUE",
+  ALERT_QUEUE_DLQ: "ALERT_QUEUE_DLQ",
+};
