47851d055159c6c191b38eceded0e3f5c1e0d6ba
mentorvikash <mentorvikash@gmail.com>
2026-05-23 22:53:15 +0530

    refactor: restructure message consumption logic in RabbitMQ consumer

--- src/rabbitmq/consumer.ts
+++ src/rabbitmq/consumer.ts
@@ -1,24 +1,29 @@
-import amqp from "amqplib";
-import { env } from "../config/env";
+import { ConsumeMessage } from "amqplib";
+import { consumerChannel } from "./connection";
 
-async function receiveMessage() {
-  const connection = await amqp.connect(env.RABBITMQ_URL);
-  const channel = await connection.createChannel();
-  const queue = "task_queue";
-
+export const consumeQueue = async function (
+  queue: string,
+  handler: (data: unknown) => Promise<void>,
+): Promise<void> {
+  // const connection = await amqp.connect(env.RABBITMQ_URL);
+  const channel = await consumerChannel();
   await channel.assertQueue(queue, { durable: true });
-  console.log(`Waiting for messages in ${queue}...`);
+  console.log("consumer queue", queue);
 
-  channel.consume(
-    queue,
-    (msg: amqp.Message | null) => {
-      if (msg) {
-        console.log(`Received: ${msg.content.toString()}`);
-        channel.ack(msg);
-      }
-    },
-    { noAck: false },
-  );
-}
-
-receiveMessage().catch(console.error);
+  await channel.consume(queue, async (message: ConsumeMessage | null) => {
+    if (!message) {
+      console.log("consumer cancelled by broker");
+      return;
+    }
+    try {
+      const parsedData = JSON.parse(message.content.toString());
+      await handler(parsedData);
+      channel.ack(message);
+    } catch (error) {
+      console.log(
+        "cosumer process error",
+        error instanceof Error ? error.message : error,
+      );
+    }
+  });
+};
